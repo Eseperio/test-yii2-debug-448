0377f26be0db169cd706d1d8f7d8d92486208ecf
E.Alamo <erosdelalamo@gmail.com>
2020-12-07 00:15:15 +0100

    Removed non useful information

--- emulated-library/src/frontend/controllers/OtherController.php
+++ emulated-library/src/frontend/controllers/OtherController.php
@@ -12,15 +12,8 @@
 namespace example\emulated\frontend\controllers;
 
 use Da\User\Controller\SettingsController as BaseSettingsController;
-use Da\User\Model\User;
-use Yii;
-use yii\base\UserException;
-use yii\helpers\ArrayHelper;
-use yii\web\ForbiddenHttpException;
-use yii\web\NotFoundHttpException;
-use yii\web\Response;
 
-class SettingsController extends BaseSettingsController
+class OtherController extends BaseSettingsController
 {
 
 
